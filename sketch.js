896972fa48aa331399643c159bcf9895e8ee59bc
Augustine-Joe <85614669+Augustine-Joe@users.noreply.github.com>
2021-08-24 18:42:50 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var student1,student2
+function setup() {
+  createCanvas(400,400);
+  student1=new Student ("Augustine",11,7)
+  student2=new Student ("Ram",99,100)
+  student2.display()
+  
+
+
+}
+
+function draw() 
+{
+  background(30);
+ // student1.display()
+  
+}
